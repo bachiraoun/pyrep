f50e6fe2e42fb8d4d1b577912e02927db4ac8026
Bach <bachir.aoun@e-aoun.com>
2015-09-29 17:52:13 -0500

    get pickling error added

--- Repository.py
+++ Repository.py
@@ -29,27 +29,33 @@ def unlock_required(func):
         return func(self, *args, **kwargs)
     return wrapper
 
-#### get pickling errors method ###
+### get pickling errors method ###
 def get_pickling_errors(obj, seen=None):
     if seen == None:
         seen = []
-    try:
+    if hasattr(obj, "__getstate__"):
         state = obj.__getstate__()
-    except AttributeError as e:
-        #state = obj.__dict__
-        return str(e)
+    #elif hasattr(obj, "__dict__"):
+    #    state = obj.__dict__
+    else:
+        return None
+    #try:
+    #    state = obj.__getstate__()
+    #except AttributeError as e:
+    #    #state = obj.__dict__
+    #    return str(e)
     if state == None:
         return 'object state is None'
     if isinstance(state,tuple):
-        if not isinstance(state[0],dict):
+        if not isinstance(state[0], dict):
             state=state[1]
         else:
             state=state[0].update(state[1])
     result = {}    
     for i in state:
         try:
-            pickle.dumps(state[i],protocol=2)
-        except pickle.PicklingError:
+            pickle.dumps(state[i], protocol=pickle.HIGHEST_PROTOCOL)
+        except pickle.PicklingError as e:
             if not state[i] in seen:
                 seen.append(state[i])
                 result[i]=get_pickling_errors(state[i],seen)
