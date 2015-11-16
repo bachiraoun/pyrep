c45a74c5063f3a052a4fd410041928911aaa4254
Bach <bachir.aoun@e-aoun.com>
2015-11-16 13:20:30 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,30 @@
-from distutils.core import setup
+import os
+from setuptools import setup
 
+PATH =  os.path.dirname(os.path.realpath(__file__))
 setup(
-  name = 'pyrep',
-  packages = ['pyrep'], # this must be the same as the name above
-  version = '1.0',
-  description = 'This is a pythonic way to organize dumping and pulling python objects and/or any other type of files to a repository.',
-  author = 'Bachir Aoun',
-  author_email = 'bachir.aoun@e-aoun.com',
-  url = 'https://github.com/bachiraoun/pyrep',
-  download_url = 'https://github.com/bachiraoun/pyrep/tree/v1.0',
-  keywords = ['repository', 'data management'], 
-  classifiers = [],
+    # Application name
+    name="pyrep",
+    # Packages
+    packages=["pyrep"],
+    # packages dir
+    package_dir={'pyrep': '.'},
+    # Version number
+    version="0.1.0",
+    # Application author details
+    author="Bachir AOUN",
+    author_email="bachir.aoun@e-aoun.com",
+    # Include additional files into the package
+    include_package_data=True,
+    # Details
+    url="https://github.com/bachiraoun/pyrep",
+    # license file
+    license="LICENSE.txt",
+    # short description
+    description='This is a pythonic way to organize dumping and pulling python objects and/or any other type of files to a repository.',
+    # long description
+    long_description=open( "README.md" ).read(),
+    # Dependent packages (distributions)
+    #install_requires=['numpy',],
+    #setup_requires=['numpy'], 
 )
